570be25b29d97da3fa9716481575fb4ca8132eb9
CzimbalmosKristof <czimbalmos.kristof@gmail.com>
2025-04-23 18:24:36 +0200

    book

--- Frontend/src/components/Book.jsx
+++ Frontend/src/components/Book.jsx
@@ -1,13 +1,14 @@
 
 
-function Book({book}) {
+function Book({book}) 
+{
     return (
         <div className="card bg-white shadow-lg rounded-lg border border-gray-200 w-96 m-4">
             <div className="card-body p-6">
-                <p className="card-title text-xl font-bold text-center text-gray-800 mb-2">{book.konyv_cim}</p>
-                <p className="text-gray-600 mb-1"><span className="font-semibold">Szerző:</span> {book.konyv_szerzo}</p>
-                <p className="text-gray-600 mb-1"><span className="font-semibold">Tartalom:</span> {book.konyv_tartalom}</p>
-                <p className={`text-sm font-semibold mb-4 ${book.konyv_status === 'Elérhető' ? 'text-green-500' : 'text-red-500'}`}>
+                <p className="card-title text-xl font-bold text-center text-gray-800 mb-2">{book.cim}</p>
+                <p className="text-gray-600 mb-1"><span className="font-semibold">Szerző:</span> {book.szerzo}</p>
+                <p className="text-gray-600 mb-1"><span className="font-semibold">Tartalom:</span> {book.tema}</p>
+                <p className={`text-sm font-semibold mb-4 ${book.elerheto === 'Elérhető' ? 'text-green-500' : 'text-red-500'}`}>
                     {book.konyv_status}
                 </p>
                 <div className="card-actions flex justify-end">
